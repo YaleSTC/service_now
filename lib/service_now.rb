bb81a62ccb161eeba6b49ba93a240b8b245052d1
hengchu <hengchu.zhang@gmail.com>
2013-07-27 16:29:38 -0400

    now updating and creating both work

--- lib/service_now.rb
+++ lib/service_now.rb
@@ -3,17 +3,29 @@ require "rest_client"
 require "json"
 
 module ServiceNow
-    $post_resource = nil
-    $get_resource = nil
     $root_url = nil
     $username = nil
     $password = nil
+
     class Configuration
+
         def self.configure(auth_hash = {})
             $root_url = auth_hash[:sn_url].sub(/(\/)+$/, '') #remove trailing slash if there are any
             $username = auth_hash[:sn_username]
             $password = auth_hash[:sn_password]
-            $post_resource = RestClient::Resource.new($root_url + "/incident.do?JSON&sysparm_action=insert", $username, $password)
+            "SN::Configuration successful"
+        end
+
+        def self.get_resource(query_hash = {})
+            # to be filled in
+        end
+
+        def self.post_resource
+            RestClient::Resource.new($root_url + "/incident.do?JSON&sysparm_action=insert", $username, $password)
+        end
+
+        def self.update_resource(incident_number)
+           RestClient::Resource.new($root_url + "/incident.do?JSON&sysparm_query=number=#{incident_number}&sysparm_action=update", $username, $password) 
         end
     end
 
@@ -21,6 +33,7 @@ module ServiceNow
 
         def initialize(attributes = {})
             @attributes = attributes
+            @saved_on_sn = false
         end
  
         def attributes
@@ -38,9 +51,19 @@ module ServiceNow
         end
 
         def save!
-            response = $post_resource.post(self.attributes.to_json)
+            # we only create new incidents if it's not saved already
+            if !@saved_on_sn
+                response = Configuration.post_resource.post(self.attributes.to_json)
+                @saved_on_sn = true
+            else
+                response = Configuration.update_resource(self.number).post(self.attributes.to_json)
+                # even though we know it's saved already, just set it again to be sure
+                @saved_on_sn = true
+            end
             hash = JSON.parse(response, { :symbolize_names => true })
-            #this is the object, and there is always only one since we're creating
+            # this is the object
+            # and there is always only one
+            # since we're creating or updating
             inc_object = hash[:records][0]
             inc_object.each do |key, value|
                 key_name = key.to_s
